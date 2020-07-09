203e4fda73e4e5ffdc9f2d391b8937ae725bb1f9
sooraj_s <sooraj1998ayiroor@gmail.com>
2020-07-09 20:54:01 +0530

    menu components

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+import Menu from './components/MenuComponent';
+function App() {
+  return (
+    <div >
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
-    );
-  }
+  );
 }
 
 export default App;
