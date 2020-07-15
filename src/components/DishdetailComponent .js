ec3721399afb866bf972284e0e67fb5cf85b63fe
sooraj_s <sooraj1998ayiroor@gmail.com>
2020-07-15 15:38:21 +0530

    Redux Thunk

--- src/components/DishdetailComponent .js
+++ src/components/DishdetailComponent .js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Col, Label  } from "reactstrap";
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 
 function RenderDish({dish}) {
@@ -38,6 +39,27 @@ function RenderDish({dish}) {
     }
 
     const DishDetail = props => {
+                    
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null) 
+        
       if (props.dish) {
           return (
             <div className="container">
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem  } from "reactstrap";
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 function RenderMenuItem ({dish, onClick}) {
     return (
@@ -16,7 +17,7 @@ function RenderMenuItem ({dish, onClick}) {
 }
 
 const Menu = (props) => {
-      const menu = props.dishes.map((dish) => {
+    const menu = props.dishes.dishes.map((dish) => {
           return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
              <RenderMenuItem dish={dish} />
@@ -24,6 +25,27 @@ const Menu = (props) => {
           );
     });
 
+    if (props.dishes.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.dishes.errMess) {
+        return(
+            <div className="container">
+                <div className="row"> 
+                    <div className="col-12">
+                        <h4>{props.dishes.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    else
       return (
         <div className="container">
             <div className="row">
