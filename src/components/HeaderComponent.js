a644acbc38970f7259f62229df61fc8dbf51e3b7
sooraj_s <sooraj1998ayiroor@gmail.com>
2020-07-15 08:32:18 +0530

    Uncontrolled Forms

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,65 +1,79 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron,Button, Modal, ModalHeader, ModalBody,
-    Form, FormGroup, Input, Label  } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, NavItem, Collapse, Jumbotron, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 export default class Header extends Component {
+
     constructor(props) {
-        super(props);
-    
+        super(props)
+
+        this.state = {
+            isNavOpen: false,
+            isModalOpen: false
+        };
+
         this.toggleNav = this.toggleNav.bind(this);
         this.toggleModal = this.toggleModal.bind(this);
         this.handleLogin = this.handleLogin.bind(this);
+    }
 
-        this.state = {
-          isNavOpen: false,
-          isModalOpen: false
-        };
-      }
+    toggleNav() {
+        this.setState({ isNavOpen: !this.state.isNavOpen });
+    }
 
-      toggleNav() {
-        this.setState({
-          isNavOpen: !this.state.isNavOpen
-        });
-      }
-      toggleModal() {
-        this.setState({
-          isModalOpen: !this.state.isModalOpen
-        });
-      }
-      handleLogin(event) {
+    toggleModal() {
+        this.setState({ isModalOpen: !this.state.isModalOpen });
+    }
+
+    handleLogin(evt){
         this.toggleModal();
-        alert("Username: " + this.username.value + " Password: " + this.password.value
-            + " Remember: " + this.remember.checked);
-        event.preventDefault();
 
+        alert(this.username.value + " " + this.password.value);
+
+        evt.preventDefault();
     }
 
     render() {
-        return(
+        return (
             <React.Fragment>
                 <Navbar dark expand="md">
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav} />
-                        <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img src="assets/images/logo.png" height="30" widtg="41" alt="Ristorance Con Fusion" />
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar >
+
                             <Nav navbar>
-                            <NavItem>
-                                <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span>  Home</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span>  About Us</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link"  to='/menu'><span className="fa fa-list fa-lg"></span>  Menu</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
-                            </NavItem>
-                            </Nav>
-                            <Nav  navbar>
                                 <NavItem>
-                                    <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home fa-lg"></span> Home
+                                </NavLink>
+                                </NavItem>
+
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-info fa-lg"></span> About us
+                                </NavLink>
+                                </NavItem>
+
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg"></span> Menu
+                                </NavLink>
+                                </NavItem>
+
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-address-card fa-lg"></span> Contact us
+                                </NavLink>
+                                </NavItem>
+                            </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}>
+                                        <span className="fa fa-sign-in fa-lg">Login</span>
+                                    </Button>
                                 </NavItem>
                             </Nav>
                         </Collapse>
@@ -69,8 +83,8 @@ export default class Header extends Component {
                     <div className="container">
                         <div className="row row-header">
                             <div className="col-12 col-sm-6">
-                                <h1>Ristorante con Fusion</h1>
-                                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+                                <h1>Ristorante Con Fusion</h1>
+                                <p>We take inspiration from the world's jlsfjldsjfljldsjllj</p>
                             </div>
                         </div>
                     </div>
@@ -83,6 +97,7 @@ export default class Header extends Component {
                                 <Label htmlFor="username">Username</Label>
                                 <Input type="text" id="username" name="username" innerRef={(input) => this.username = input } />
                             </FormGroup>
+
                             <FormGroup>
                                 <Label htmlFor="password">Password</Label>
                                 <Input type="password" id="password" name="password" innerRef={(input) => this.password = input } />
@@ -98,7 +113,7 @@ export default class Header extends Component {
                         </Form>
                     </ModalBody>
                 </Modal>
-            </React.Fragment>      
+            </React.Fragment>
         );
     }
 }
