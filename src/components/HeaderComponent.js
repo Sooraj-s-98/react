3aa88802efb7698ca963b0aa2a72c5c9012462fa
sooraj_s <sooraj1998ayiroor@gmail.com>
2020-07-15 11:25:44 +0530

    React Redux Form Validation

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -84,7 +84,7 @@ export default class Header extends Component {
                         <div className="row row-header">
                             <div className="col-12 col-sm-6">
                                 <h1>Ristorante Con Fusion</h1>
-                                <p>We take inspiration from the world's jlsfjldsjfljldsjllj</p>
+                                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
                             </div>
                         </div>
                     </div>
